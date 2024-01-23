0312c3ba1b066808e81eba1686952bf5a62a4f44
Yamileth Reveca <yamilethcaldera@gmail.com>
2024-01-23 00:12:00 -0300

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: true,
-
-  //APIURL:"http://localhost:8080"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
 
-  //APIURL:"http://localhost:8080"
 };
 
 /*
