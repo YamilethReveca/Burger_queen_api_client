17f101da04fbb3ee9e6c3003bb3d78251634506b
Yamileth Reveca <yamilethcaldera@gmail.com>
2024-01-23 00:07:03 -0300

    agregando localStorage y servicio de autenticacion.

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/auth.service';// inyeccion del servicio
 
 @Component({
   selector: 'app-login',
@@ -17,24 +18,32 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService: AuthService // inyeccion del servicio en el constructor
   ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  enviarFormulario() {
 
-    if (this.formulario && this.formulario.get('email')) {
+  enviarFormulario(): void {
 
-      if (this.formulario.valid) {
-        this.router.navigate(['pedidos']);
+    const { email, clave } = this.formulario.value;
+
+    this.authService.login(email, clave).subscribe({
+
+      next: (response: { accessToken: string }) => {
+        if (response && response.accessToken) {
+          localStorage.setItem("accessToken", response.accessToken);
+          this.router.navigate(['pedidos']);
+        } else {
+          console.error('Error de autenticación');
+          // Puedes agregar lógica para mostrar un mensaje de error al usuario en tu componente
+        }
+      },
+      error: (error: any) => {
+        console.error('Error de autenticación:', error);
+        // Puedes agregar lógica para mostrar un mensaje de error al usuario en tu componente
       }
-    }
+    });
   }
 }
-
-
-
-
-
