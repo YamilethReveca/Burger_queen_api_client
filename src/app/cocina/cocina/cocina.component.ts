98c1eb2ae98dfc168be949e040f78c897dfe39bf
Yamileth Reveca <yamilethcaldera@gmail.com>
2024-02-19 21:56:15 -0300

    gregando hora del pedido hora de delivering del pedido

--- src/app/cocina/cocina/cocina.component.ts
+++ src/app/cocina/cocina/cocina.component.ts
@@ -22,7 +22,14 @@ export class CocinaComponent implements OnInit {
   obtenerOrdenesCocina(): void {
     this.authService.obtenerOrdenesCocina().subscribe(
       (ordenes: any[]) => {
-        this.ordenes = ordenes;
+        // Iterar sobre las órdenes para agregar la hora del pedido y la hora de lista
+        this.ordenes = ordenes.map(orden => {
+          return {
+            ...orden,
+            horaPedido: this.formatDate(new Date(orden.dateEntry)),
+            horaLista: orden.status === 'delivering' ? this.formatDate(new Date()) : null
+          };
+        });
       },
       (error) => {
         console.error('Error al obtener las órdenes de cocina:', error);
@@ -42,6 +49,30 @@ export class CocinaComponent implements OnInit {
       }
     );
   }
+
+  private formatDate(date: Date): string {
+    // Formatea la fecha y hora en formato legible
+    return date.toLocaleString();
+  }
+
+
+  // Función para calcular el tiempo transcurrido
+calcularTiempoTranscurrido(horaPedido: string, horaFinalizacion: string): string {
+  // Parsear las horas en objetos de fecha
+  const fechaPedido = new Date(horaPedido);
+  const fechaFinalizacion = new Date(horaFinalizacion);
+
+  // Calcular la diferencia de tiempo en milisegundos
+  const diferenciaTiempo = fechaFinalizacion.getTime() - fechaPedido.getTime();
+
+  // Calcular los minutos y segundos
+  const minutos = Math.floor(diferenciaTiempo / 60000); // 1 minuto = 60000 milisegundos
+  const segundos = Math.floor((diferenciaTiempo % 60000) / 1000); // 1 segundo = 1000 milisegundos
+
+  // Formatear la salida
+  return `${minutos} minutos ${segundos} segundos`;
+}
+  
 }
 
 
